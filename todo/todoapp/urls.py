3b1e0978e6f35c07cb7d8b80f3f3188c74820d2e
GeorgeJiss <jerinjiss001@gmail.com>
2025-01-06 23:35:58 +0530

    completed

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home-page'),
     path('register/', views.register, name = 'register'),      
-    path('login/', views.login, name = 'login'),
-    path('add/', views.add_task, name='add-task'),       
-    path('delete/<str:task_name>/', views.delete_task, name='delete-task'),
-    path('update/<str:task_name>/', views.update_task, name='update-task'),
+    path('login/', views.loginpage, name = 'login'),
+    # path('add/', views.add_task, name='add-task'),       
+    path('delete/<str:task_name>/', views.delete, name='delete'),
+    path('update/<str:task_name>/', views.update, name='update'),
     path('logout/', views.logout_user, name='logout'),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,29 +1,78 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
-from .models import Task  # Assuming Task is your model
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
+from . models import Task
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def home(request):
-    tasks = Task.objects.all()
-    return render(request, 'todoapp/todo.html', {})
+    if request.method == 'POST':
+        task = request.POST.get('task')
+        new_todo = Task(user=request.user, todo_name = task)
+        new_todo.save()
+
+    all_todos = Task.objects.filter(user=request.user)
+    context = {
+        'todos' : all_todos
+    }
+    return render(request, 'todoapp/todo.html', context)
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('home-page')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if len(password) < 3:
+            messages.error(request, 'Password must be atleast 3 characterrs')
+            return redirect('register')
+        
+        #If user already exists
+        get_all_users_by_username = User.objects.filter(username=username)
+        if get_all_users_by_username:
+            messages.error(request, 'Error, username already exists')
+            return redirect('register')
+
+        new_user = User.object.createuser(username=username, email=email, password=password)
+        new_user.save()
+        messages.success(request, 'User successfully created, login now!')
     return render(request, 'todoapp/register.html', {})
 
-def login(request):
+def loginpage(request):
+    if request.user.is_authenticated:
+        return redirect('home-page')
+    if request.method == 'POST':
+        username = request.POST.get('uname')
+        password = request.POST.get('pass')
+
+        validate_user = authenticate(username=username, password=password)
+        if validate_user is not None:
+            login(request, validate_user)
+            return redirect('home-page')
+        else:
+            messages.error(request, 'Error, wrong user details or user does not exist')
+            return redirect('login')
+
     return render(request, 'todoapp/login.html', {})
 
-def add_task(request):
+def add(request):
     if request.method == 'POST':
         task_name = request.POST.get('task')
         if task_name:
             Task.objects.create(todo_name=task_name, status=False)
         return redirect('home-page')
 
-def delete_task(request, task_name):
+@login_required
+def delete(request, task_name):
     Task.objects.filter(todo_name=task_name).delete()
     return redirect('home-page')
 
-def update_task(request, task_name):
+@login_required
+def update(request, task_name):
     task = Task.objects.get(todo_name=task_name)
     task.status = not task.status
     task.save()
@@ -31,4 +80,4 @@ def update_task(request, task_name):
 
 def logout_user(request):
     logout(request)
-    return redirect('home-page')
+    return redirect('login')
